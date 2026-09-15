b41e0a82a472ac2ee72aa512a4593fffded204ba
Nitin Dhyani <121809257+nitindhyani01@users.noreply.github.com>
2026-09-15 20:14:47 +0530

    Sync GFG - Reverse a Stack (Medium)

---
+++ GFG/Medium/0_ReverseAStack.java
@@ -0,0 +1,28 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/reverse-a-stack/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+class Solution
+{ 
+    static void reverseStack(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+
+        int top = stack.pop();
+        reverseStack(stack);  
+        
+        Stack<Integer> temp = new Stack<>();
+        while (!stack.isEmpty()) {
+            temp.push(stack.pop());
+        }
+
+        stack.push(top);
+
+        while (!temp.isEmpty()) {
+            stack.push(temp.pop());
+        }
+    }
+}
