fda2a39272e4fb070e179ae8102b39e24e780654
Nitin Dhyani <121809257+nitindhyani01@users.noreply.github.com>
2026-09-16 19:03:40 +0530

    Sync GFG - Dominant Pairs (Easy)

---
+++ GFG/Easy/0_DominantPairs.java
@@ -0,0 +1,36 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/dominant-pairs/1
+ * Platform     : GFG
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public int dominantPairs(int[] arr) {
+        int n = arr.length;
+        int mid = n / 2;
+
+        int[] firstHalf = new int[mid];
+        for (int i = 0; i < mid; i++) {
+            firstHalf[i] = arr[i];
+        }
+        java.util.Arrays.sort(firstHalf);
+
+        int[] secondHalf = new int[n - mid];
+        for (int i = mid; i < n; i++) {
+            secondHalf[i - mid] = arr[i];
+        }
+        java.util.Arrays.sort(secondHalf);
+
+        int count = 0;
+        int j = 0;
+
+        for (int i = 0; i < mid; i++) {
+            while (j < secondHalf.length && firstHalf[i] >= 5 * secondHalf[j]) {
+                j++;
+            }
+            count += j;
+        }
+
+        return count;
+    }
+}
