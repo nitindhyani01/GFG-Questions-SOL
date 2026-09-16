d3bc8ab26549426f2f19865d06fffabf9bcbeae0
Nitin Dhyani <121809257+nitindhyani01@users.noreply.github.com>
2026-09-16 18:05:36 +0530

    Sync GFG - Start Coding (Basic)

---
+++ GFG/Basic/0_StartCoding.java
@@ -0,0 +1,12 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/start-coding-1/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+class GFG {
+    public static void main(String[] args) {
+        // code here
+        System.out.print("Hello World");
+    }
+}
