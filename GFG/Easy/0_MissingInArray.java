71f01b2be55fdc2ec19136a89c93001437cedbd5
Nitin Dhyani <121809257+nitindhyani01@users.noreply.github.com>
2026-09-21 16:37:11 +0530

    Sync GFG - Missing in Array (Easy)

---
+++ GFG/Easy/0_MissingInArray.java
@@ -0,0 +1,19 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/missing-number-in-array1416/1
+ * Platform     : GFG
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    int missingNum(int arr[]) {
+        int n = arr.length + 1; 
+
+        long expectedSum = (long) n * (n + 1) / 2;
+        long actualSum = 0;
+
+        for (int num : arr) {
+            actualSum += num;
+        }
+        return (int) (expectedSum - actualSum);
+    }
+}
